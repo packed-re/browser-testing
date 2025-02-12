3db26e8547d28c8645a0278414b085578fa6245b
packed-re <149999642+packed-re@users.noreply.github.com>
2025-02-12 09:41:36 +0200

    u

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 print = console.log; // im over it
 
 cookieStore.addEventListener("change", (event) => {
-	console.log(event);
+	print(event);
 });
 
-document.cookie = "merp=123";
+document.cookie = "merp=mainPath";
+document.cookie = "merp=diffPath; path=/merpington";
+document.cookie = "merp=diffDomain; domain=github.io";
 print("done")
