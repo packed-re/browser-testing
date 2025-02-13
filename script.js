84a966562e44ef625b29fd2b4e4c880b24820734
packed-re <149999642+packed-re@users.noreply.github.com>
2025-02-13 13:08:25 +0200

    u

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 print = console.log; // im over it
 
-cookieStore.addEventListener("change", (event) => {
-	print(event);
-});
+if(cookieStore !== undefined)
+	cookieStore.addEventListener("change", (event) => {
+		print(event);
+	});
+else
+	print("cookieStore doesnt exist (firefox moment)");
 
 document.cookie = "merp=mainPath";
 document.cookie = "merp=diffPath; path=/merpington";
 document.cookie = "merp=diffDomain; domain=github.io";
-print("done")
+print("done");
